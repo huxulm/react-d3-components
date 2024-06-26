48d2a0bef8a707b6d1d4003e558fe42cae6926e9
Huxulm <huxulm@gmail.com>
2024-06-26 20:01:17 +0800

    update

--- src/excises/d3-array/blur/index.stories.tsx
+++ src/excises/d3-array/blur/index.stories.tsx
@@ -5,7 +5,7 @@ const Path = lazy(() => import("."));
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Components/Path1",
+  title: "Excises/d3-array/blur",
   component: Path,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
