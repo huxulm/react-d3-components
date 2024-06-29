080e2d8f37b77862d506b8ee2b74c758db0eb1ab
Huxulm <huxulm@gmail.com>
2024-06-29 10:35:20 +0800

    fix import

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: 'NonExistingPath'
+      external: "NonExistingPath",
+    },
+  },
+  server: {
+    fs: {
+      cachedChecks: false
     }
   },
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
+});
