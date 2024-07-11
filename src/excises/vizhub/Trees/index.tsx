82c263643526a7070828cc679501f1e963d1413d
Huxulm <huxulm@gmail.com>
2024-07-11 16:25:20 +0800

    add transitions to trees

--- src/excises/vizhub/Trees/index.tsx
+++ src/excises/vizhub/Trees/index.tsx
@@ -1,30 +1,136 @@
-import { hierarchy, tree } from "d3-hierarchy";
+import { RefObject, useCallback, useEffect, useRef, useState } from "react";
+import { hierarchy, tree, cluster } from "d3-hierarchy";
+import { HierarchyPointLink, HierarchyPointNode } from "d3-hierarchy";
 import { data, DataShape } from "./data";
-import { RefObject, useCallback, useEffect, useRef } from "react";
-import { main } from "./viz";
+import {
+  LinksSelection,
+  main,
+  MainSelection,
+  NodesSelection,
+  Sizes,
+  transitionToRadialCluster,
+  transitionToRadialTree,
+} from "./viz";
 
+type LayoutType = "radial tree" | "radial cluster";
 export const Trees = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const radialTreeFn = useCallback(
-    (container: RefObject<HTMLDivElement>) => {
-      if (!container.current) {
-        return [null, null];
-      }
-      const width = container.current?.clientWidth;
-      const height = container.current?.clientHeight;
-      const radius = Math.min(width, height) / 2;
-      const layout = tree<DataShape>()
-        .size([2 * Math.PI, radius])
-        .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+  // selections
+  const mainRef = useRef<MainSelection>(null);
+  const nodesRef = useRef<NodesSelection>(null);
+  const linksRef = useRef<LinksSelection>(null);
 
-      const nodes = layout(hierarchy<DataShape>(data));
-      return [nodes.descendants(), nodes.links()];
+  // transition druation
+  const [duration, setDuration] = useState(1000);
+
+  const [layoutType, setLayoutType] = useState<LayoutType>("radial tree");
+  const getSize = useCallback(
+    (ref: RefObject<HTMLDivElement>): Sizes => {
+      const width = ref.current!.clientWidth;
+      const height = ref.current!.clientHeight;
+      return { width, height, radius: Math.min(width, height) / 2 };
     },
     [ref]
   );
-  
+  const radialTreeFn = useCallback((): [
+    HierarchyPointNode<DataShape>[],
+    HierarchyPointLink<DataShape>[],
+    Sizes,
+  ] => {
+    const sizes = getSize(ref);
+    const layout = tree<DataShape>()
+      .size([2 * Math.PI, sizes.radius])
+      .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+    const nodes = layout(hierarchy<DataShape>(data));
+    return [nodes.descendants(), nodes.links(), sizes];
+  }, [ref, getSize]);
+
+  const radialTreeClusterFn = useCallback((): [
+    HierarchyPointNode<DataShape>[],
+    HierarchyPointLink<DataShape>[],
+    Sizes,
+  ] => {
+    const sizes = getSize(ref);
+    const layout = cluster<DataShape>()
+      .size([2 * Math.PI, sizes.radius])
+      .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+    const nodes = layout(hierarchy<DataShape>(data));
+    return [nodes.descendants(), nodes.links(), sizes];
+  }, [ref, getSize]);
+
+  // 初始化
   useEffect(() => {
-    const [nodes, links] = radialTreeFn(ref)
-    main(ref, { nodes: nodes as any, links: links as any })}, [ref]);
-  return <div style={{ width: "100vw", height: "100vh" }} ref={ref} />;
+    const [nodes, links, sizes] = radialTreeFn();
+    const [MainSelection, NodesSelection, LinksSelection] = main(ref, {
+      nodes,
+      links,
+      sizes,
+    });
+    mainRef.current = MainSelection;
+    nodesRef.current = NodesSelection;
+    linksRef.current = LinksSelection;
+  }, [ref]);
+  const changeToRadialCluster = () => {
+    setLayoutType("radial cluster");
+    transitionToRadialCluster(
+      radialTreeClusterFn,
+      mainRef.current,
+      nodesRef.current,
+      linksRef.current,
+      duration
+    );
+  };
+  const changeToRadial = () => {
+    setLayoutType("radial tree");
+    transitionToRadialTree(
+      radialTreeFn,
+      mainRef.current,
+      nodesRef.current,
+      linksRef.current,
+      duration
+    );
+  };
+  return (
+    <div style={{ width: "100vw", height: "100vh" }} ref={ref}>
+      <div
+        style={{
+          position: "absolute",
+          top: 50,
+          left: 50,
+          display: "flex",
+          flexDirection: "column",
+          border: "1px dotted black",
+          padding: 5,
+        }}
+      >
+        <label>
+          <label>过渡时间(0~2s)</label>
+          <input
+            type="range"
+            value={duration}
+            min={0}
+            max={2000}
+            onChange={(e) => setDuration(parseFloat(e.target.value))}
+          />
+        </label>
+        <label onClick={changeToRadial}>
+          <input
+            type="radio"
+            id="radial tree"
+            checked={layoutType === "radial tree"}
+            value={"radial tree"}
+          />
+          <label>径向树</label>
+        </label>
+        <label onClick={changeToRadialCluster}>
+          <input
+            type="radio"
+            checked={layoutType === "radial cluster"}
+            value={"radial cluster"}
+          />
+          <label>径向聚类树</label>
+        </label>
+      </div>
+    </div>
+  );
 };
--- src/excises/vizhub/Trees/viz.ts
+++ src/excises/vizhub/Trees/viz.ts
@@ -2,73 +2,189 @@ import { RefObject } from "react";
 import { HierarchyPointNode, HierarchyPointLink } from "d3-hierarchy";
 import { linkRadial } from "d3-shape";
 import { zoom } from "d3-zoom";
-import { select } from "d3-selection";
+import { select, Selection } from "d3-selection";
 import { scaleSequential } from "d3-scale";
 import { DataShape } from "./data";
 import { interpolateRainbow } from "d3-scale-chromatic";
 
+export interface Sizes {
+  width: number;
+  height: number;
+  radius: number;
+}
 interface VizProps {
   links: HierarchyPointLink<DataShape>[];
   nodes: HierarchyPointNode<DataShape>[];
+  sizes: Sizes;
 }
+export type MainSelection = Selection<
+  SVGGElement,
+  null,
+  HTMLDivElement | null,
+  unknown
+> | null;
+export type NodesSelection = Selection<
+  SVGCircleElement,
+  HierarchyPointNode<DataShape>,
+  SVGGElement,
+  null
+> | null;
+export type LinksSelection = Selection<
+  SVGPathElement,
+  HierarchyPointLink<DataShape>,
+  SVGGElement,
+  null
+> | null;
+
+const radialDiagonal = linkRadial<HierarchyPointNode<DataShape>, any, any>()
+  .angle((d) => d.x)
+  .radius((d) => d.y);
+
+// color fn
+const colorScale = scaleSequential(interpolateRainbow);
+const colorFn = (n: number) => (_: any, i: number) => colorScale(i / n);
 
 export const main = (
   container: RefObject<HTMLDivElement>,
-  { links, nodes }: VizProps
-) => {
-  if (!container.current) {
-    return;
-  }
-  const width = container.current.clientWidth;
-  const height = container.current.clientHeight;
+  { links, nodes, sizes }: VizProps
+): [MainSelection, NodesSelection, LinksSelection] => {
+  const { width, height } = sizes;
   const cx = width * 0.5; // adjust as needed to fit
   const cy = height * 0.5; // adjust as needed to fit
   const svg = select(container.current)
     .selectAll("svg")
     .data([null])
     .join("svg")
-    .attr("viewBox", [-cx, -cy, width, height])
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${cx}, ${cy})`);
 
   // add zoom
   const zoomG = svg.append("g");
-  svg.call(
-    zoom().on("zoom", (event) => {
-      zoomG.attr("transform", event.transform);
-    }) as any
-  );
+  const zoomBehavior = zoom().on("zoom", (event) => {
+    zoomG.attr("transform", event.transform)
+  });
 
-  // color fn
-  const colorScale = scaleSequential(interpolateRainbow);
-  const colorFn = (n: number) => (_: any, i: number) => colorScale(i / n);
+  svg.call(zoomBehavior as any);
 
   // append links
-  zoomG
+  const Links = zoomG
     .append("g")
     .selectAll("path")
     .data(links)
-    .join("path")
+    .enter()
+    .append("path")
     .attr("stroke", colorFn(links.length))
     .attr("stroke-width", 2)
     .attr("fill", "none")
-    .attr(
-      "d",
-      linkRadial<HierarchyPointNode<DataShape>, any, any>()
-        .angle((d) => d.x)
-        .radius((d) => d.y) as any
-    );
+    .attr("d", radialDiagonal as any);
 
   // append nodes
-  zoomG
+  const Nodes = zoomG
     .append("g")
     .selectAll("circle")
     .data(nodes)
-    .join("circle")
+    .enter()
+    .append("circle")
     .attr(
       "transform",
       (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y}, 0)`
     )
-    .attr("fill", "black")
-    .attr("r", 2.5);
+    .attr("fill", "white")
+    .attr("stroke", colorFn(links.length))
+    .attr("r", 20);
+  return [
+    svg as MainSelection,
+    Nodes as NodesSelection,
+    Links as LinksSelection,
+  ];
 };
+
+export function transitionToRadialTree(
+  fn: () => [
+    HierarchyPointNode<DataShape>[],
+    HierarchyPointLink<DataShape>[],
+    Sizes,
+  ],
+  mainG: MainSelection,
+  nodesG: NodesSelection,
+  linksG: LinksSelection,
+  duration: number
+) {
+  const [nodes, links, sizes] = fn();
+  mainG!
+    .transition()
+    .duration(duration)
+    .attr(
+      "transform",
+      "translate(" + sizes.width / 2 + "," + sizes.height / 2 + ")"
+    );
+  // set appropriate translation (origin in middle of svg)
+
+  linksG!
+    .data(links)
+    .transition()
+    .duration(duration)
+    .style("stroke", colorFn(links.length))
+    .attr("d", radialDiagonal as any); //get the new radial path
+
+  nodesG!
+    .data(nodes)
+    .transition()
+    .duration(duration)
+    .attr(
+      "transform",
+      (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y}, 0)`
+    );
+
+  nodesG!
+    .select("circle")
+    .transition()
+    .duration(duration)
+    .style("stroke", "#984ea3");
+}
+
+export function transitionToRadialCluster(
+  fn: () => [
+    HierarchyPointNode<DataShape>[],
+    HierarchyPointLink<DataShape>[],
+    Sizes,
+  ],
+  mainG: MainSelection,
+  nodesG: NodesSelection,
+  linksG: LinksSelection,
+  duration: number
+) {
+  const [nodes, links, sizes] = fn();
+  mainG!
+    .transition()
+    .duration(duration)
+    .attr(
+      "transform",
+      "translate(" + sizes.width / 2 + "," + sizes.height / 2 + ")"
+    );
+  // set appropriate translation (origin in middle of svg)
+
+  linksG!
+    .data(links)
+    .transition()
+    .duration(duration)
+    .style("stroke", colorFn(links.length))
+    .attr("d", radialDiagonal as any); //get the new radial path
+
+  nodesG!
+    .data(nodes)
+    .transition()
+    .duration(duration)
+    .attr(
+      "transform",
+      (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y}, 0)`
+    );
+
+  nodesG!
+    .select("circle")
+    .transition()
+    .duration(duration)
+    .style("stroke", "#4daf4a");
+}
